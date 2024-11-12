cc969d9d524cf75c8a8f795159372f4a994c7900
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-11-13 01:58:01 +0530

    done

---
+++ JavaScript/Introduction_javascript/variabledemo.js
@@ -0,0 +1,21 @@
+// To store values we can use variables
+// variables are memory bucket that stores our values and has a name as well.
+
+// How to create variable in js
+// -3 ways to create variable
+// 1)- using var keyword
+
+var marks=90;
+var score=5;
+console.log(marks,score);
+
+// 2)- using let keyword
+
+let age=25;
+let flag=0;
+console.log(age,flag);
+
+// 3)- using const keyword
+
+const pi=3.14;
+console.log(pi);
