37486b679bfd90b4bed1d7faf75ea1485981c7fb
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-12-20 18:36:33 +0530

    done

---
+++ JavaScript/Introduction_javascript/consoledemo.js
@@ -0,0 +1,5 @@
+console.log(10);
+console.log("hello world");
+
+let age=25;
+console.log(age);
---
+++ JavaScript/Introduction_javascript/datatypesDemo.js
@@ -0,0 +1,8 @@
+var marks=100;
+var name="sanchit";
+var age;
+var comapny= null;
+var salary= undefined;
+var isStudent= false;
+
+console.log(marks, name,age,comapny,salary,isStudent);
